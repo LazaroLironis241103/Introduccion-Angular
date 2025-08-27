af5aaa8a30ced777b26f8f259716ad7d0ddc8946
Lazaro Lironis <99282167+LazaroLironis241103@users.noreply.github.com>
2025-08-27 20:04:23 +0000

    .

--- Angular/app-angular/src/app/app.ts
+++ Angular/app-angular/src/app/app.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from './nuevo-componente/nuevo-componente';
 import { ComponenteEnLineaComponent } from './componente-en-linea/componente-en-linea.component';
+import { Padre } from "./padre/padre";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteEnLineaComponent, Padre],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
-  protected readonly titulo = 'Hola Mundo desde Angular';
+  protected readonly titulo = 'Tienda Online';
 }
---
+++ Angular/app-angular/src/app/padre/hijo/hijo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  standalone: true,
+  imports: [],
+  templateUrl: './hijo.html',
+  styleUrl: './hijo.css'
+})
+export class Hijo {
+
+}
